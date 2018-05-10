acf3b09a4bb9116716b3a4502a809a49234ad257
Rodrigo <b.rodrigotorres@gmail.com>
2018-05-10 20:56:23 -0300

    UI Agregado a MapVC

--- Pixel-City/Pixel-City/Controller/MapVC.swift
+++ Pixel-City/Pixel-City/Controller/MapVC.swift
@@ -7,22 +7,19 @@
 //
 
 import UIKit
-import Alamofire
-import AlamofireImage
-
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnCenterWasPressed(_ sender: Any) {
+        
     }
-
-
+    
 }
 
